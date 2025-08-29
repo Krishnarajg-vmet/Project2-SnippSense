d2bd5056a8d0682cf703bd9c31eca1217e77f6d9
Krishnaraj <krishnaraj.vmet@gmail.com>
2025-08-29 10:45:39 +0530

    Commit for Initial Project Setup

---
+++ snippsense/src/main/java/com/kay/snippsense/SnippsenseApplication.java
@@ -0,0 +1,13 @@
+package com.kay.snippsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippsenseApplication.class, args);
+	}
+
+}
---
+++ snippsense/src/test/java/com/kay/snippsense/SnippsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kay.snippsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
